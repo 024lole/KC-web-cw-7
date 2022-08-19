e6c7aaa225a3b64a4487d6677affe6a1280bb55c
024lole <noorralhazzaa@gmail.com>
2022-08-19 23:01:02 +0300

    tried my best to do it all >3<

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+function BMI (weigh,height) {
+
+ let result = weight / (height*height);
+return result 
+}
+ console.log (BMI(80,1.8))
+
+
+function Status (BMI) {
+ if (BMI < 18.5 ) {
+    return "لديك نقص بالوزن "
+ } else if (BMI >= 18.5 && BMI<25) {
+ } else  (BMI>=25) 
+    return "لديك زيادة بالوزن"
+   }
+ console.log(BMI(80,1.8))
+
+function calculate () {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi = BMI (weight.height)
+    let desc = Status(BMI(weight,height))
+    let div = document.getElementById("result")
+    div.innerHTML = bmi + "==" + desc
+}
+
 
